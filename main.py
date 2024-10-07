edb7f8586a7055f68712e0b73d96986b580b1df2
999gawkboyy <mingeun3669@gmail.com>
2024-10-08 00:44:14 +0900

    add main page demo

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
 from direct.showbase.ShowBase import ShowBase
-import complexpbr
-from panda3d.core import DirectionalLight, AmbientLight, LVector3
-from direct.filter.CommonFilters import CommonFilters
-from panda3d.core import PointLight, Vec3, Vec4
-import light
+from direct.gui.DirectGui import *
 
-class Game(ShowBase):
+class MyApp(ShowBase):
     def __init__(self):
-        super().__init__()
+        ShowBase.__init__(self)
 
-        light.light()
+        self.create_screen_1()
 
-        self.blackhole = self.loader.load_model("./assets/blackhole/scene.bam")
-        self.blackhole.reparent_to(base.render)
-        self.blackhole.set_pos(0, 0, 0) 
+    def create_screen_1(self):
+        """첫 번째 화면 생성"""
+        self.clear_screen()
 
-        self.camera.set_pos(0, -10, 0)  # 카메라를 모델 뒤쪽으로 이동
-        
-        env_light_1 = PointLight('env_light_1')
-        env_light_1.set_color(Vec4(Vec3(6),1))
-        env_light_1 = base.render.attach_new_node(env_light_1)
-        env_light_1.set_pos(0,0,0)
+        self.label1 = DirectLabel(text="This is Screen 1",
+                                  scale=0.1,
+                                  pos=(0, 0, 0.8))
 
-        self.space = self.loader.load_model("./assets/space/space.bam")
-        self.space.reparent_to(base.render)
-        self.space.set_pos(0, 0, 0)
-        self.space.set_scale(0.5)
-        self.space.set_light(env_light_1)
+        self.button1 = DirectButton(text=("Go to Screen 2"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.4),
+                                    command=self.create_screen_2)
 
-        # complex PBR 셰이더 적용
-        complexpbr.apply_shader(self.render)
+        self.button2 = DirectButton(text=("Go to Screen 3"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.2),
+                                    command=self.create_screen_3)
 
-        # 화면 공간 효과 초기화
-        complexpbr.screenspace_init()
-        complexpbr.apply_shader(self.render, intensity=0.9, env_cam_pos=None, env_res=256)
-       
+    def create_screen_2(self):
+        """두 번째 화면 생성"""
+        self.clear_screen()
 
-        # 블룸 효과 설정
-        screen_quad = base.screen_quad
-        screen_quad.set_shader_input("bloom_intensity", 2.0)
+        self.label2 = DirectLabel(text="This is Screen 2",
+                                  scale=0.1,
+                                  pos=(0, 0, 0.8))
 
-        self.task_mgr.add(self.rotate_blackhole)
+        self.button1 = DirectButton(text=("Go to Screen 1"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.4),
+                                    command=self.create_screen_1)
 
-    def rotate_blackhole(self, task):
-        # 매 프레임마다 모델을 회전
-        self.blackhole.set_h(self.blackhole.get_h() + 1)  # 1도씩 회전
-        return task.cont  # 계속해서 작업을 수행
+        self.button2 = DirectButton(text=("Go to Screen 3"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.2),
+                                    command=self.create_screen_3)
 
-app = Game()
+    def create_screen_3(self):
+        """세 번째 화면 생성"""
+        self.clear_screen()
+
+        self.label3 = DirectLabel(text="This is Screen 3",
+                                  scale=0.1,
+                                  pos=(0, 0, 0.8))
+
+        self.button1 = DirectButton(text=("Go to Screen 1"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.4),
+                                    command=self.create_screen_1)
+
+        self.button2 = DirectButton(text=("Go to Screen 2"),
+                                    scale=0.1,
+                                    pos=(0, 0, 0.2),
+                                    command=self.create_screen_2)
+
+    def clear_screen(self):
+        """현재 화면에 있는 모든 GUI 요소를 제거"""
+        for child in self.aspect2d.getChildren():
+            child.removeNode()
+
+app = MyApp()
 app.run()
